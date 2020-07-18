b949cdd18a46726d3a3ca752f4c727cc18e14be0
lotar94 <lotar.94@gmail.com>
2020-07-18 04:25:33 -0400

    Update

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+measurement = ['sin Informacion']
 
 @app.route('/')
 def hello():
-    return "Hello, world"
+    return 'Hola que hace'
 
-if __name__=='__main__':
-    app.run()
+@app.route('/getData')
+def getData():
+    return measurement[0]
 
+@app.route('/lastMeasurement', methods=['POST'])
+def saveLastMeasurement():
+    data = request.json
+    measurement[0] = data
+    print(measurement)
+    return jsonify({"message": "data ingresa exitosamente"})
 
-
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)

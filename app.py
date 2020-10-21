eb9a37bf145db8e3529fcfaccd8fa13ab75f076d
Lotar <lotar.m@inreality.com>
2020-10-21 01:41:00 -0300

    Se crea archivo para almacenar datos

--- app.py
+++ app.py
@@ -1,23 +1,40 @@
 from flask import Flask, request, jsonify
+import os.path
 
 app = Flask(__name__)
 
-measurement = ['sin Informacion']
+measurement = ['sin Information']
 
 @app.route('/')
 def hello():
-    return 'Hola que hace'
+    return 'Hola que hace!!'
 
-@app.route('/getData')          
+
+@app.route('/getData')
 def getData(): return measurement[0]
 
 
 @app.route('/lastMeasurement', methods=['POST'])
 def saveLastMeasurement():
     data = request.json
+    print("1")
+    validateUser(str(data["user_id"]))
+    print("3")
     measurement[0] = data
-    print(measurement)
     return jsonify({"message": "data ingresa exitosamente"})
+
+
+def validateUser(userId):
+    print("El user id: " + str(userId))
+    if os.path.isfile(userId+".csv"):
+        print("Existe")
+    else:
+        print("No existe")
+        f = open(userId+".csv", "w+")
+        for i in range(10):
+            f.write("This is line %d\r\n" % (i + 1))
+    print("2")
+
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
